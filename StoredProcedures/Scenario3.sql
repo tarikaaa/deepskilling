949689d2e8cd0a4f4038e45f32e273d630edce94
tarikaaa <727723eucd059@skcet.ac.in>
2026-07-14 21:37:49 +0530

    PL/SQL exercises

---
+++ StoredProcedures/Scenario3.sql
@@ -0,0 +1,45 @@
+CREATE OR REPLACE PROCEDURE TransferFunds
+(
+    p_FromAccount IN NUMBER,
+    p_ToAccount IN NUMBER,
+    p_Amount IN NUMBER
+)
+IS
+    v_Balance NUMBER;
+BEGIN
+
+    SELECT Balance
+    INTO v_Balance
+    FROM Accounts
+    WHERE AccountID = p_FromAccount;
+
+    IF v_Balance >= p_Amount THEN
+
+        UPDATE Accounts
+        SET Balance = Balance - p_Amount
+        WHERE AccountID = p_FromAccount;
+
+        UPDATE Accounts
+        SET Balance = Balance + p_Amount
+        WHERE AccountID = p_ToAccount;
+
+        COMMIT;
+
+        DBMS_OUTPUT.PUT_LINE('Fund transfer successful.');
+
+    ELSE
+
+        DBMS_OUTPUT.PUT_LINE('Insufficient Balance.');
+
+    END IF;
+
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        DBMS_OUTPUT.PUT_LINE('Account not found.');
+
+    WHEN OTHERS THEN
+        ROLLBACK;
+        DBMS_OUTPUT.PUT_LINE(SQLERRM);
+
+END;
+/
