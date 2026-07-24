5899464f3cd0ad9dde5512c579d74cfe941058e0
tarikaaa <727723eucd059@skcet.ac.in>
2026-07-24 10:04:40 +0530

    Update AssertionsTest

--- JUnitDemo/src/test/java/com/example/AssertionsTest.java
+++ JUnitDemo/src/test/java/com/example/AssertionsTest.java
@@ -8,19 +8,19 @@ public class AssertionsTest {
     @Test
     public void testAssertions() {
 
-        // Assert equals
+       
         assertEquals(5, 2 + 3);
 
-        // Assert true
+        
         assertTrue(5 > 3);
 
-        // Assert false
+       
         assertFalse(5 < 3);
 
-        // Assert null
+        
         assertNull(null);
 
-        // Assert not null
+        
         assertNotNull(new Object());
     }
 }
