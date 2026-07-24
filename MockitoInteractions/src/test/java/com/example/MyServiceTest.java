77dc1280951afa010738205a674738fd2b380355
tarikaaa <727723eucd059@skcet.ac.in>
2026-07-24 11:45:27 +0530

    completed mockito exercise

---
+++ MockitoInteractions/src/test/java/com/example/MyServiceTest.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import static org.mockito.Mockito.*;
+
+import org.junit.Test;
+
+public class MyServiceTest {
+
+    @Test
+    public void testVerifyInteraction() {
+
+        
+        ExternalApi mockApi = mock(ExternalApi.class);
+
+        MyService service = new MyService(mockApi);
+
+        
+        service.fetchData();
+
+        
+        verify(mockApi).getData();
+    }
+}
